d463cee78706ee3d1d2dc2cb6f844787932f4b21
CosmoStas <stasbkj@gmail.com>
2020-12-02 21:13:44 +0300

    Line Done

---
+++ VectorPaint/Line.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VectorPaint
+{
+    public class Line : IFigure
+    {
+        public Point BeginLine;
+        public Point EndLine;
+
+        Line()
+        {
+
+        }
+        Line(Point firstLocation, Point lastLocation)
+        {
+            BeginLine = firstLocation;
+            EndLine = lastLocation;
+        }
+
+        public List<Point> GetPoints()
+        {
+            return new List<Point>(2) { BeginLine, EndLine };
+        }
+
+        public void SetPoints(Point firstLocation, Point lastLocation)
+        {
+            BeginLine = firstLocation;
+            EndLine = lastLocation;
+        }
+    }
+}
